dd07b3973941e39e3ed80935f501c23aaf0f21a7
selvincephus <selvincephus@gmail.com>
2019-05-30 11:08:55 +0200

    In order to use the trained model in an application two new classes have been added.

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,26 +1,17 @@
 ###################################################################################################
 #  Written by Selvin Cephus Jayakumar
-#  Processing the sparql queries to be "learnable" in training.
-#  The following tokens were created,
-#  http://dbpedia.org/resource/ - dbpedia resource
-#  http://dbpedia.org/ontology/ - dbpedia ontology
-#  http://www.w3.org/1999/02/22-rdf-syntax-ns#type - 22rdfsyntaxnstype
-#  http://dbpedia.org/property/ - dbpedia property
-#  < - starturl
-#  > - endurl
-#  ( - openbrace
-#  ) - closebrace
 ###################################################################################################
-
-import urllib.parse as urlparse
+import unicodedata
 import re
-import string
+
+import torch
+
 
 class SparqlPostprocessing:
+    # Legacy code. Is not used currently but could be a useful class for future work
     def __init__(self):
         pass
-        # query = "SELECT DISTINCT ?uri WHERE openbrace ?uri closebrace starturl dbpedia ontology creator endurl starturl dbpedia resource Bill Finger endurl . ?uri starturl rdfsyntaxnstype endurl starturl dbpedia ontology ComicsCharacter endurl"
-        # sparql_tokens = query.split(' ')
+
     def sparqilise(self, query):
         query = query.replace("starturl", " <")
         query = query.replace("endurl", "> ")
@@ -40,4 +31,111 @@ class SparqlPostprocessing:
             query = query.replace("dbpedia property", "http://dbpedia.org/property/")
 
         return query
+
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {"humidity":2,"solve":3,"alarm":4,"features":5,"LAA":6,"CDA":7,"counters":8, "value":9, "package":10}
+        self.word2count = {}
+        self.index2word = {0:"SOS",1:"EOS",2:"humidity",3:"solve",4:"alarm",5:"features",6:"LAA",7:"CDA",8:"counters",9:"value",10:"package"}
+
+        self.n_words = len(self.word2index)  # Count SOS and EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+
+class DataPrep:
+    def __init__(self, lang1, lang2, reverse, device):
+        self.SOS_token = 0
+        self.EOS_token = 1
+        self.lang1 = lang1
+        self.lang2 = lang2
+        self.reverse = reverse
+        self.device = device
+
+    # Turn a Unicode string to plain ASCII, thanks to
+    # http://stackoverflow.com/a/518232/2809427
+    def unicodeToAscii(s):
+        # print(s)
+        return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+
+    def normalizeString(self, s):
+        # s = unicodeToAscii(s.lower().strip())
+        s = re.sub(r"([.!?])", r" \1", s)
+        # s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+        return s
+
+    def readLangs(self):
+        print("Reading lines...")
+        # Read the file and split into lines
+
+        data = open('data/%s-%s.txt' % (self.lang1, self.lang2), encoding="utf8"). \
+            read().strip().split('\n')
+
+        # Split every line into pairs and normalize
+        pairs = [[self.normalizeString(s) for s in l.split(',')] for l in data]
+
+        # Reverse pairs, make Lang instances
+        if self.reverse:
+            pairs = [list(reversed(p)) for p in pairs]
+            input_lang = Lang(self.lang2)
+            output_lang = Lang(self.lang1)
+        else:
+            input_lang = Lang(self.lang1)
+            output_lang = Lang(self.lang2)
+
+        return input_lang, output_lang, pairs
+
+    def prepareData(self):
+        input_lang, output_lang, pairs = self.readLangs()
+        print("Read %s train sentence pairs" % len(pairs))
+        print("Trimmed to %s train sentence pairs" % len(pairs))
+        print("Counting words...")
+        for pair in pairs:
+            if len(pair) == 2:
+                input_lang.addSentence(pair[0])
+                output_lang.addSentence(pair[1])
+        print("Counted words in train set:")
+        print(input_lang.name, input_lang.n_words)
+        print(output_lang.name, output_lang.n_words)
+        return input_lang, output_lang, pairs
+
+    def normalizeString(self, s):
+        # s = unicodeToAscii(s.lower().strip())
+        s = re.sub(r"([.!?])", r" \1", s)
+        # s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+        return s
+
+    def indexesFromSentence(self, lang, sentence):
+        indexes = []
+        # return [lang.word2index[word] for word in sentence.split(' ')]
+        words = sentence.split(' ')
+        words = list(filter(None, words))  # filter out null characters in sentences. Causes key error if not done.
+        for word in words:
+            word_index = lang.word2index[word]
+            indexes.append(word_index)
+        return indexes
+
+    def tensorFromSentence(self, lang, sentence):
+        indexes = self.indexesFromSentence(lang, sentence)
+        indexes.append(self.EOS_token)
+        return torch.tensor(indexes, dtype=torch.long, device=self.device).view(-1, 1)
+
+    def tensorsFromPair(self, pair):
+        input_tensor = self.tensorFromSentence(self.input_lang, pair[0])
+        target_tensor = self.tensorFromSentence(self.output_lang, pair[1])
+        return input_tensor, target_tensor
+
 
---
+++ eng2sparql_translator.py
@@ -0,0 +1,176 @@
+###################################################################################################
+#  Written by Selvin Cephus Jayakumar
+###################################################################################################
+import re
+import torch
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from data_preprocess import DataPrep
+
+MAX_LENGTH = 50
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+SOS_token = 0
+EOS_token = 1
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        self.gru.flatten_parameters()
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+dt_prep = DataPrep('eng', 'sparql', False, device)
+input_lang, output_lang, pairs = dt_prep.prepareData()
+
+hidden_size = 1024
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+# encoder1 = torch.load('encoder-decoder/encoder1_minproc', map_location='cpu')
+# attn_decoder1 = torch.load('encoder-decoder/attn_decoder1_minproc', map_location='cpu')
+encoder1 = torch.load('encoder_decoder/encoder1_minproc')
+attn_decoder1 = torch.load('encoder_decoder/attn_decoder1_minproc')
+
+def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = dt_prep.tensorFromSentence(input_lang, sentence)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                                     encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
+
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            else:
+                decoded_words.append(output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words, decoder_attentions[:di + 1]
+
+def call_to_sparql_endpoint(query):
+    # query = "SELECT DISTINCT ?n WHERE { <http://dbpedia.org/resource/Steinsee> dbo:maximumDepth ?n . }"
+    print(query)
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setReturnFormat(JSON)
+
+    # sparql.setQuery(query)  # the previous query as a literal string
+
+    # resp = sparql.query().convert()
+    resp = sparql.query()
+    resp_dict = resp.convert()
+    value = resp_dict['results']['bindings']
+    print('Subject       Predicate        Object')
+    for index in range(len(value)):
+        subject = value[index]['s']
+        predicate = value[index]['p']
+        object = value[index]['o']
+        # print('{} {} {}'.format(subject, predicate, object))
+        if index == 100:
+            return True
+        # for s, p, o in value[index].items():
+        #    print('{} {} {}'.format(s, p, o))
+    # print('Sparql query respone: ', value)
+
+
+def evaluateAndShowAttention(input_sentence):
+    input_sentence = dt_prep.normalizeString(input_sentence)
+    # sparqliser = SparqlPostprocessing()
+    output_words, attentions = evaluate(
+        encoder1, attn_decoder1, input_sentence)
+    print('input =', input_sentence)
+    print('output =', ' '.join(output_words))
+    sparql_output = ' '.join(output_words)
+    # sparql_query = sparqliser.sparqilise(sparql_output)
+    sparql_query = re.sub(r'\<EOS\>', '', sparql_output)
+    call_to_sparql_endpoint(sparql_query)
+
+evaluateAndShowAttention("Which comic characters are painted by Bill Finger?")
