7d32cafb4f5826e9f05031b71d88c110347ced98
selvincephus <selvincephus@gmail.com>
2019-06-04 21:47:31 +0200

    changes

--- data_parser.py
+++ data_parser.py
@@ -5,28 +5,39 @@
 import json
 import csv
 import time
-import urllib.parse as urlparse
 import re
 import string
 
-start = time.time()
+# start = time.time()
+#
+# with open('data/lcquad_qaldformat.json',encoding="utf8") as datafile:
+#     data = json.load(datafile)
+#     output_data = []
+#     for idx, question in enumerate(data['questions']):
+#         eng = question['question'][0]['string']
+#
+#         sparql = question['query']['sparql']
+#         eng_sparql = [eng, sparql]
+#         output_data.append(eng_sparql)
+#
+# with open('eng-sparql_tab.txt', mode='w',newline='',encoding="utf8") as engsparql_file:
+#     file_writer = csv.writer(engsparql_file, delimiter=',')
+#     for q in output_data:
+#         file_writer.writerow({q[0]+'\t'+q[1]})
+#
+# end = time.time()
+# print(end - start)
 
-with open('data/lcquad_qaldformat.json',encoding="utf8") as datafile:
-    data = json.load(datafile)
-    output_data = []
-    for idx, question in enumerate(data['questions']):
-        eng = question['question'][0]['string']
+# import csv
 
-        sparql = question['query']['sparql']
-        eng_sparql = [eng, sparql]
-        output_data.append(eng_sparql)
+in_file = r"data\eng-sparql.txt"
+out_file = r"eng-sparql_tab1.txt"
 
-with open('eng-sparql.txt', mode='w',newline='',encoding="utf8") as engsparql_file:
-    file_writer = csv.writer(engsparql_file, delimiter=',')
-    for q in output_data:
-        file_writer.writerow([q[0], q[1]])
-
-end = time.time()
-print(end - start)
+with open(in_file, mode="r", encoding="utf8") as in_text:
+    in_reader = csv.reader(in_text, delimiter = ',')
+    with open(out_file, mode="w", encoding="utf8") as out_csv:
+        out_writer = csv.writer(out_csv, delimiter='\t')
+        for row in in_reader:
+            out_writer.writerow(row)
 
 
